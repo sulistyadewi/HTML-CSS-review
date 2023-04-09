e6e4909678b59dc499397baa94dcba6735743c57
sulistyadewi <sulistya2coding@gmail.com>
2023-04-09 15:36:19 +0700

    menambahkan semua

---
+++ dist/script.js
@@ -0,0 +1,8 @@
+
+let menuBar = document.querySelector("#menuBar")
+let menuMobile = document.querySelector("#menuMobile")
+
+menuBar.addEventListener("click", function(){
+    menuMobile.classList.toggle("-top-52")
+    menuMobile.classList.toggle("top-12")
+})
