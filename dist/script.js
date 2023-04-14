6efa424f145916676f080b899231718b15c5cb35
sulistyadewi <sulistya2coding@gmail.com>
2023-04-14 15:15:00 +0700

    m

--- dist/script.js
+++ dist/script.js
@@ -3,6 +3,6 @@ let menuBar = document.querySelector("#menuBar")
 let menuMobile = document.querySelector("#menuMobile")
 
 menuBar.addEventListener("click", function(){
-    menuMobile.classList.toggle("-top-52")
+    menuMobile.classList.toggle("top-52")
     menuMobile.classList.toggle("top-12")
 })
